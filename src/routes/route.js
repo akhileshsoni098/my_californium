2533f5bcc1448ff39dd59e9300d6859679c3f33c
Akhilesh Soni <akhileshsoni098@gmail.com>
2022-12-12 16:51:30 +0530

    missingNumber assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -12,4 +12,36 @@ router.get('/test-you', function(req, res){
 })
 
 
+router.get('/sol1', function(req, res){
+    
+ let arr= [1,2,3,5,6,7]
+let sum = arr.length+1
+let s = sum*(sum+1)/2
+ let a = 0
+ for(let i = 0; i<arr.length; i++){
+a +=arr[i]
+ }
+let missingNumber = (s-a)
+
+    res.send(`your missing number ${missingNumber} `)
+})
+
+
+router.get('/sol2', function(req, res){
+   
+ let arr= [33, 34, 35, 37, 38]
+ // console.log(arr[arr.length-1])
+  let sum = (arr.length+1)*(arr[0] + arr[arr.length-1])  //n(last +first)/2
+ //  console.log(sum) 
+  let s = sum/2
+  let a = 0
+ for(let i = 0; i<arr.length; i++){
+ a += arr[i]
+ }
+  let missingNo = (s-a)
+ 
+    res.send(`your missing number ${missingNo}`)
+})
+
+
 module.exports = router;
