b3183799e38e60d5d83e72927c86fa3da2ade4d1
Akhilesh Soni <akhileshsoni098@gmail.com>
2022-12-13 23:13:52 +0530

    done this task

--- src/routes/route.js
+++ src/routes/route.js
@@ -11,5 +11,71 @@ router.get('/test-you', function(req, res){
     res.send("very important text")
 })
 
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ]
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ]
+       },
+   ]
+
+//    let obj = {
+//     "name" : "akj",
+//      "dob": "01/03/1999",
+//      "gender" : "male",
+//      "city": "json",
+//      "sports": ["football"]
+//     }
+   
+//     function func (players){
+//        let count = 0
+//        for(let i =0; i<players.length; i++){
+//            if(players[i].name == obj.name){
+//            count++
+//        }
+//     } if(count==0){
+//        players.push(obj)
+//     }return players
+//    }
+    
+//     console.log(func(players))
+
+router.post('/players', function(req, res){
+   let newObj = req.body
+   for(let i=0; i<players.length; i++){
+     if(players[i].name == newObj.name){
+       return res.send(`Dear User Your Name Is Already Exist`)
+     }  
+        } players.push(newObj)
+
+         res.send({data: players , status: true})
+    })
+
+    
+
+
 
 module.exports = router;
