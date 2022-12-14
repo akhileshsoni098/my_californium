153a3c7671fd2c2f13c4ba186f5a14fecb45eddf
Akhilesh Soni <akhileshsoni098@gmail.com>
2022-12-14 22:46:15 +0530

    task completed successfully

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
+
 const route = require('./routes/route.js');
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use('/', route);
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+app.listen(3000, function() {
+    console.log('Express app running on port ' + 3000)
 });
+
+
+// A || B
--- src/routes/route.js
+++ src/routes/route.js
@@ -159,5 +159,72 @@ router.post( "/post-query-2", function (req, res){
     res.send( {data: finalArr , status: true})
 })
 
+ /*
+ ASSIGNMENT:
+you will be given an array of persons ( i.e an array of objects )..each person will have 
+ a {name: String , age: Number, votingStatus: true/false(Boolean)}
+take input in query param as votingAge..and for all the people above that age, change votingStatus as true
+also return an array consisting of only the person that can vote
+
+WRITE A POST API TO THE ABOVE
+
+
+take this as sample for array of persons:
+*/
+
+let persons= [
+   {
+   name: "PK",
+   age: 10,
+   votingStatus: false
+},
+{
+   name: "SK",
+   age: 20,
+   votingStatus: false
+},
+{
+   name: "AA",
+   age: 70,
+   votingStatus: false
+},
+{
+   name: "SC",
+   age: 5,
+   votingStatus: false
+},
+{
+   name: "HO",
+   age: 40,
+   votingStatus: false
+}
+]
+
+// let a = 22
+// let nage = []   
+// for(let i=0; i<persons.length; i++ ){
+//     if(persons[i].age>a){
+//        persons[i].votingStatus = true
+//     }
+// } 
+
+//   nage.push(persons)
+// console.log(nage)
+
+router.post( "/votingAge", function (req, res){
+    let a = req.query.age
+    let nage = [] 
+    for(let i=0; i<persons.length; i++ ){
+        if(persons[i].age>=a){
+           persons[i].votingStatus = true
+           nage.push(persons[i]) 
+        }
+    } 
+           res.send({persons:nage,status:true} ) 
+   
+})
+
+
+
 
 module.exports = router;
