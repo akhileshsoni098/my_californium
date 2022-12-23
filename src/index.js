1086d88f7dfbcf5314da55a83b67693d524e8fc6
Akhilesh Soni <akhileshsoni098@gmail.com>
2022-12-23 13:41:06 +0530

    done this task

--- src/index.js
+++ src/index.js
@@ -7,8 +7,24 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const akhileshMW= function (req, res, next) {
+    var currentdate = new Date(); 
+    var datetime =  currentdate.getDate() + " "
+                    + (currentdate.getMonth()+1)  + " " 
+                    + currentdate.getFullYear() + "  "  
+                    + currentdate.getHours() + ":"  
+                    + currentdate.getMinutes() + ":" 
+                    + currentdate.getSeconds();
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+    let ip= req.ip
+    let url= req.originalUrl
+    console.log(`${datetime}  ${ip}  ${url}`)
+    next()    
+}
+
+app.use( akhileshMW )
+
+mongoose.connect("mongodb+srv://akhilesh912:GilRK3PuHjI3u7Et@cluster0.cio4vrk.mongodb.net/akhilesh-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
